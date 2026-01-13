3ead39133e975371025154fc83c2580c5ca32269
Codeurdu34 <alexandre.lab@hotmail.fr>
2026-01-13 16:49:54 +0100

    Ajout de la définition des composants UI réutilisables

---
+++ Front/src/app/shared/components/ui/tag-input/tag-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-input',
+  imports: [],
+  templateUrl: './tag-input.html',
+  styleUrl: './tag-input.css',
+})
+export class TagInput {
+
+}
