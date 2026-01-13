e7dffdaccbceb7e325635f9fdb93fb0e230e0fb1
alphonso94 <gestionsitealpha@gmail.com>
2026-01-13 12:01:39 +0100

    dockerfile

--- Front/src/app/app.ts
+++ Front/src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./composant/navbar/navbar";
 import { Footer } from './composant/footer/footer';
-import { Container } from "./composant/container/container";   
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer, Container],
+  imports: [RouterOutlet, Navbar, Footer,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
